8414911f459df373c63ede97f4dac5b4a100bb9a
Clyde Broderick <clyderick3@gmail.com>
2022-04-07 19:26:48 -0400

    Started Testie Collections.

---
+++ gettestie/src/main/java/rocks/zipcode/TestArrayList.java
@@ -0,0 +1,24 @@
+package rocks.zipcode;
+
+import java.util.ArrayList;
+
+public class TestArrayList<T> {
+
+    //I believe these are the most common:
+    //Size
+    //Get
+    //Add
+
+    private ArrayList<T> list = new ArrayList<>();
+
+    public ArrayList<T> getList() {
+        return list;
+    }
+
+    public void initList(T[] arr){
+        list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++){
+           list.add(arr[i]);
+        }
+    }
+}
---
+++ gettestie/src/test/java/rocks/zipcode/TestArrayList.java
@@ -0,0 +1,15 @@
+package rocks.zipcode;
+
+import org.junit.Test;
+
+public class TestArrayList {
+
+    @Test
+    public void TestArrayListTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+}
