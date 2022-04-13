461b60caba9e3bac787f063e5596deaebbca0d4e
Clyde Broderick <clyderick3@gmail.com>
2022-04-13 12:36:44 -0400

    Added two hashMap tests.

---
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -0,0 +1,35 @@
+package rocks.zipcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    @Test
+    public void addHashMapTest(){
+        //Given
+        Map<String, String> hashMap = new HashMap();
+        Integer expected = 1;
+        //When
+        hashMap.put("Mike","Jones");
+        Integer actual = hashMap.size();
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void removeHashMapTest(){
+        //Given
+        Map<String, String> hashMap = new HashMap();
+        Integer expected = 0;
+        //When
+        hashMap.put("Mike","Jones");
+        hashMap.remove("Mike");
+        Integer actual = hashMap.size();
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+}
