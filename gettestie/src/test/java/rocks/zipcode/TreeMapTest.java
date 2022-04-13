aff8b84183e28768d122beff7ce5e941bdba8b37
Clyde Broderick <clyderick3@gmail.com>
2022-04-13 14:53:08 -0400

    Created addTreeMapTest.

--- gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -1,6 +1,23 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeMap;
+
 public class TreeMapTest {
 
-
+    @Test
+    public void addTreeMapTest(){
+        //Given
+        TreeMap<Integer, String> tinder = new TreeMap<>();
+        //When
+        tinder.put(1, "Kiss");
+        tinder.put(2, "Hug");
+        tinder.put(3, "High Five.");
+        Integer expected = 3;
+        Integer actual = tinder.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
