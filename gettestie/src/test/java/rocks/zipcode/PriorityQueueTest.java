ffaf696fd9a18a490e63c95a52b989ceb32a5a87
Clyde Broderick <clyderick3@gmail.com>
2022-04-13 16:07:38 -0400

    Completed removePriorityQueueTest.

--- gettestie/src/test/java/rocks/zipcode/PriorityQueueTest.java
+++ gettestie/src/test/java/rocks/zipcode/PriorityQueueTest.java
@@ -8,7 +8,7 @@ import java.util.PriorityQueue;
 public class PriorityQueueTest {
 
     @Test
-    public void addPriorityQueueTest(){
+    public void addPriorityQueueTest() {
         //Given
         PriorityQueue<String> lines = new PriorityQueue<String>();
         //When
@@ -17,6 +17,21 @@ public class PriorityQueueTest {
         Integer expected = 2;
         Integer actual = lines.size();
         //Then
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removePriorityQueueTest() {
+        //Given
+        PriorityQueue<String> lines = new PriorityQueue<String>();
+        //When
+        lines.add("Person 1");
+        lines.add("Person 2");
+        //Person 2 was a fugitive that was caught.
+        lines.remove("Person 2");
+        Integer expected = 1;
+        Integer actual = lines.size();
+        //Then
+        Assert.assertEquals(expected, actual);
     }
 }
