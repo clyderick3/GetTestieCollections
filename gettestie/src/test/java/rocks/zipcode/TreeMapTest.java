60c666b161ef23eb70d2827a86616896ba4fde78
Clyde Broderick <clyderick3@gmail.com>
2022-04-13 14:56:02 -0400

    Duplicated methods and finished removeTreeMapTest.

--- gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -20,4 +20,60 @@ public class TreeMapTest {
         //Then
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void removeTreeMapTest(){
+        //Given
+        TreeMap<Integer, String> tinder = new TreeMap<>();
+        //When
+        tinder.put(1, "Kiss");
+        tinder.put(2, "Hug");
+        tinder.remove(2);
+        Integer expected = 2;
+        Integer actual = tinder.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addTreeMapTest(){
+        //Given
+        TreeMap<Integer, String> tinder = new TreeMap<>();
+        //When
+        tinder.put(1, "Kiss");
+        tinder.put(2, "Hug");
+        tinder.put(3, "High Five.");
+        Integer expected = 3;
+        Integer actual = tinder.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addTreeMapTest(){
+        //Given
+        TreeMap<Integer, String> tinder = new TreeMap<>();
+        //When
+        tinder.put(1, "Kiss");
+        tinder.put(2, "Hug");
+        tinder.put(3, "High Five.");
+        Integer expected = 3;
+        Integer actual = tinder.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addTreeMapTest(){
+        //Given
+        TreeMap<Integer, String> tinder = new TreeMap<>();
+        //When
+        tinder.put(1, "Kiss");
+        tinder.put(2, "Hug");
+        tinder.put(3, "High Five.");
+        Integer expected = 3;
+        Integer actual = tinder.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
